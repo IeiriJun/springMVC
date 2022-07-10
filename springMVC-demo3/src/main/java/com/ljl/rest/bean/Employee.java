147cb6e2a86cb4ff23a179e5dd234ac8badda345
IeiriJun <IeiriJun@qq.com>
2022-07-10 22:57:45 +0800

    springMVC-demo3 commit

---
+++ springMVC-demo3/src/main/java/com/ljl/rest/bean/Employee.java
@@ -0,0 +1,68 @@
+package com.ljl.rest.bean;
+
+import java.util.Objects;
+
+public class Employee {
+
+    private Integer id;
+    private String lastName;
+    private String email;
+    //1 male, 0 female
+    private Integer gender;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Integer getGender() {
+        return gender;
+    }
+
+    public void setGender(Integer gender) {
+        this.gender = gender;
+    }
+
+    public Employee() {
+    }
+
+    public Employee(Integer id, String lastName, String email, Integer gender) {
+        this.id = id;
+        this.lastName = lastName;
+        this.email = email;
+        this.gender = gender;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Employee employee = (Employee) o;
+        return Objects.equals(id, employee.id) && Objects.equals(lastName, employee.lastName) && Objects.equals(email, employee.email) && Objects.equals(gender, employee.gender);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, lastName, email, gender);
+    }
+
+}
